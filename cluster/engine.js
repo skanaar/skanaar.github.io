809e53433958b28601b76aed6dd74da07c9c9291
Daniel Kallin <daniel.kallin@tradedoubler.com>
2014-02-05 15:24:15 +0100

    separated simulation from visualization

---
+++ cluster/engine.js
@@ -0,0 +1,182 @@
+
+function Engine(canvasId, nodes, _options){
+	var options = {
+		fps: _options.fps || 30,
+		selectEntity: _options.selectEntity || function (){},
+		deselectEntity: _options.deselectEntity || function (){}
+	}
+	var canvas = document.getElementById(canvasId)
+	var g = skanaar.Canvas(canvas, {
+		mousedown: onMouseDown, 
+		mouseup: onMouseUp, 
+		mousemove: onMouseMove
+	})
+	var offset = { x: canvas.width/2, y: canvas.height/2 }
+	var scale = { x: 0.3, y: 0.3 }
+	var selectedEntity = undefined
+	var clickedEntity = undefined
+	var mouseDownPos = undefined
+	var mouseDownOffset = offset
+
+	var entities = nodes.entities
+	var relations = nodes.relations
+
+	pulse(draw, 1000/options.fps)
+
+	function onMouseDown(pos){
+		var e = pickEntity(pos)
+		if (e){
+			clickedEntity = e
+		} else {
+			mouseDownPos = pos
+			mouseDownOffset = _.clone(offset)
+		}
+	}
+
+	function onMouseMove(pos){
+		if (mouseDownPos)
+			offset = diff(add(mouseDownOffset, pos), mouseDownPos)
+	}
+
+	function onMouseUp(pos){
+		var e = pickEntity(pos)
+		if (clickedEntity && e){
+			if (clickedEntity === e){
+				options.selectEntity(e)
+				selectedEntity = e
+			}
+			else
+				nodes.addRelation(clickedEntity, e)
+		}
+		
+		if (_.isEqual(mouseDownPos, pos)){
+			options.deselectEntity()
+			selectedEntity = undefined
+		}
+		mouseDownPos = undefined
+		clickedEntity = undefined
+	}
+
+	function pulse(action, delay){
+		var counter = 0
+		function tick(){
+			action(counter)
+			counter++
+			setTimeout(tick, delay)
+		}
+		setTimeout(tick, delay)
+	}
+
+	function transform(pos){
+		return { x: (pos.x - offset.x)/scale.x, y: (pos.y - offset.y)/scale.y }
+	}
+	function untransform(pos){
+		return { x: scale.x*pos.x + offset.x, y: scale.y*pos.y + offset.y }
+	}
+
+	function drawEntities(){
+		g.ctx.lineWidth = 10
+		_.each(entities, function (e){
+			g.ctx.fillStyle = 'rgba(255, 255, 255, 0.75)'
+			g.circle(e.x, e.y, e.r+10).fill()
+			g.ctx.fillStyle = g.colorNorm(e.charge.r, e.charge.g, e.charge.b, 0.5)
+			g.circle(e.x, e.y, e.r).fill()
+		})
+	}
+
+	function drawRelations(){
+		var margin = 10
+		g.ctx.lineWidth = 5
+		g.ctx.strokeStyle = 'rgba(255, 255, 255, 0.5)'
+		relations.each(function (e1, e2){
+			var v = normalize(diff(e1, e2))
+			var p1 = [e1.x - v.x*(e1.r+margin), e1.y - v.y*(e1.r+margin)]
+			var p2 = [e2.x + v.x*(e2.r+margin), e2.y + v.y*(e2.r+margin)]
+			g.path([p1, p2]).stroke()
+		})
+	}
+
+	function withPickedEntity(pos, action){
+		var e = pickEntity(pos)
+		if (e)
+			action(e)
+	}
+
+	function pickEntity(pos){
+		var p = transform(pos)
+		var e = _.min(entities, function (e){ return dist(e, p) })
+		return (dist(e, p) < e.r + 10) ? e : undefined
+	}
+
+	var swe = [[104,496],[80,494],[75,471],[80,461],[55,418],[55,389],[61,382],[69,356],[75,340],
+	[70,323],[80,311],[67,294],[67,245],[81,214],[101,218],[102,201],[94,198],[108,138],[120,132],
+	[133,86],[156,53],[159,60],[166,37],[194,43],[201,12],[258,61],[264,151],[232,153],[213,190],
+	[223,206],[207,229],[162,264],[150,303],[154,337],[181,354],[178,366],[163,393],[143,403],
+	[143,448],[129,475],[108,477],[104,496]]
+
+	function draw(tick){
+		nodes.simulate()
+
+		g.background(150, 150, 150)
+
+		g.ctx.save()
+		g.ctx.translate(offset.x, offset.y)
+		g.ctx.scale(scale.x, scale.y)
+
+		withPickedEntity(g.mousePos(), function (e){
+			g.ctx.fillStyle = g.color255(0,0,0,0.25)
+			g.circle(e.x, e.y, e.r+20).fill()
+		})
+
+		if (clickedEntity){
+			var p = transform(g.mousePos())
+			var d = dist(clickedEntity, p)
+			var v = normalize(diff(p, clickedEntity))
+			if (d>10){
+				var path = _.times(Math.round(d/10), function (i){
+					return [
+						clickedEntity.x + i*v.x*10 + 10*v.y*Math.cos(i+tick/2),
+						clickedEntity.y + i*v.y*10 - 10*v.x*Math.cos(i+tick/2)
+					]
+				})
+				//g.path([[clickedEntity.x, clickedEntity.y], [mx, my]]).stroke()
+				g.ctx.strokeStyle = g.color255(255, 255, 255,0.5)
+				g.ctx.lineWidth = 13
+				g.path(path).stroke()
+			}
+		}
+
+		drawRelations()
+		drawEntities()
+
+		if (selectedEntity){
+			var e = selectedEntity
+			g.ctx.strokeStyle = g.color255(0,0,0,0.5)
+			g.ctx.lineWidth = 10/scale.x
+			var sidebarEdge = transform({x:g.width()-200, y:0})
+			g.path([[e.x + e.r + 5/scale.x, e.y], [sidebarEdge.x, e.y]]).stroke()
+		}
+
+		g.ctx.restore()
+	}
+
+	return {
+		unstabilize: function (t){
+			natsep = _.random(150, 250)
+		},
+		zoom: function(direction){
+			repeat(function(strength){
+				var f = 1 - 0.03*strength
+				scale.x *= direction > 0 ? f : 1/f
+				scale.y *= direction > 0 ? f : 1/f
+			})
+		},
+		fillScreen: function(){
+			var w = canvas.parentElement.offsetWidth
+			canvas.setAttribute('width', w)
+			canvas.setAttribute('height', w*3/4)
+			offset.x = g.width()/2
+			offset.y = g.height()/2
+		}
+	}
+}
--- cluster/nodes.js
+++ cluster/nodes.js
@@ -1,33 +1,29 @@
-var natsep = 200
 var rep = 25
 var springF = 2
 var dampening = 0.95
 
-function initNodes(canvasId, _options){
-	var options = {
-		fps: _options.fps || 30,
-		selectEntity: _options.selectEntity || function (){},
-		deselectEntity: _options.deselectEntity || function (){}
-	}
-	var canvas = document.getElementById(canvasId)
-	var g = skanaar.Canvas(canvas, {
-		mousedown: onMouseDown, 
-		mouseup: onMouseUp, 
-		mousemove: onMouseMove
-	})
-	var offset = { x: canvas.width/2, y: canvas.height/2 }
-	var scale = { x: 0.3, y: 0.3 }
-	var selectedEntity = undefined
-	var clickedEntity = undefined
-	var mouseDownPos = undefined
-	var mouseDownOffset = offset
+function sq(x){ return x*x }
+function dist(a,b){ return mag(diff(a,b)) }
+function add(a,b){ return { x: a.x + b.x, y: a.y + b.y } }
+function diff(a,b){ return { x: a.x - b.x, y: a.y - b.y } }
+function mult(v,factor){ return { x: factor*v.x, y: factor*v.y } }
+function mag(v){ return Math.sqrt(sq(v.x) + sq(v.y)) }
+function normalize(v){ return mult(v, 1/mag(v)) }
 
-	var entities = _.times(80, Entity)
+function repeat(action, repetitions){
+	var r = repetitions || 10
+	_.times(r, function (i){
+		setTimeout(function (){ action(i/r) }, 50*i)
+	})
+}
+
+function Nodes(count){
+	var entities = _.times(count, Entity)
 	var relations = fillTree(Relation, 3, entities.length)
 	relations.each = function (action){
 		_.each(relations, function (r){
-			action(entities[r.start], entities[r.end])
-			action(entities[r.end], entities[r.start])
+			action(entities[r.start], entities[r.end], r)
+			action(entities[r.end], entities[r.start], r)
 		})
 	}
 
@@ -37,7 +33,6 @@ function initNodes(canvasId, _options){
 	})
 
 	_.times(150, simulate)
-	pulse(draw, 1000/options.fps)
 
 	function fillTree(factory, branches, max){
 		var accumulator = []
@@ -55,7 +50,25 @@ function initNodes(canvasId, _options){
 		return {
 			start: i,
 			end: j,
+			strength: 0.5 + Math.random(),
+			length: _.random(100,250),
 			properties: {}
+		}
+	}
+
+	function Entity(i, x, y){
+		var r = _.random(20,30)
+		return {
+			id: i,
+			name: randomName(),
+			description: randomName() + ' ' + _.times(15, randomName).join(' ').toLowerCase(),
+			x: x || _.random(-100, 100),
+			y: y || _.random(-100, 100),
+			fx: 0,
+			fy: 0,
+			r: r,
+			properties: {},
+			charge: colorObject(Math.random(), 1, 1)
 		}
 	}
 
@@ -66,83 +79,16 @@ function initNodes(canvasId, _options){
 		return _.sample(conso).toUpperCase() + _.times(_.random(2,4), syllable).join('')
 	}
 
-	function Entity(i, x, y){
-		var r = _.random(20,30)
+	function colorObject(hue, sat, lit){
+		function component(v){
+			var x = Math.cos(6.283*v)/2 + 0.5
+			return lit*(1-sat + sat*x*x)
+		}
 		return {
-			id: i,
-			name: randomName(),
-			x: x || _.random(-100, 100),
-			y: y || _.random(-100, 100),
-			fx: 0,
-			fy: 0,
-			r: r,
-			properties: {},
-			charge: g.colorObjHSL(Math.random(), 1, 1)
+			r: component(hue),
+			g: component(hue-1/3),
+			b: component(hue+1/3)
 		}
-	}
-
-	function onMouseDown(pos){
-		var e = pickEntity(pos)
-		if (e){
-			clickedEntity = e
-		} else {
-			mouseDownPos = pos
-			mouseDownOffset = _.clone(offset)
-		}
-	}
-
-	function onMouseMove(pos){
-		if (mouseDownPos)
-			offset = diff(add(mouseDownOffset, pos), mouseDownPos)
-	}
-
-	function onMouseUp(pos){
-		var e = pickEntity(pos)
-		if (clickedEntity && e){
-			if (clickedEntity === e){
-				options.selectEntity(e)
-				selectedEntity = e
-			}
-			else
-				relations.push(Relation(clickedEntity.id, e.id))
-		}
-		
-		if (_.isEqual(mouseDownPos, pos)){
-			options.deselectEntity()
-			selectedEntity = undefined
-		}
-		mouseDownPos = undefined
-		clickedEntity = undefined
-	}
-
-	function repeat(action, repetitions){
-		var r = repetitions || 10
-		for(var i=r; i>0; i--)
-			setTimeout(function (){ action(1-i/r) }, 20*i)
-	}
-
-	function pulse(action, delay){
-		var counter = 0
-		function tick(){
-			action(counter)
-			counter++
-			setTimeout(tick, delay)
-		}
-		setTimeout(tick, delay)
-	}
-
-	function sq(x){ return x*x }
-	function dist(a,b){ return mag(diff(a,b)) }
-	function add(a,b){ return { x: a.x + b.x, y: a.y + b.y } }
-	function diff(a,b){ return { x: a.x - b.x, y: a.y - b.y } }
-	function mult(v,factor){ return { x: factor*v.x, y: factor*v.y } }
-	function mag(v){ return Math.sqrt(sq(v.x) + sq(v.y)) }
-	function normalize(v){ return mult(v, 1/mag(v)) }
-	function transform(pos){
-		return { x: (pos.x - offset.x)/scale.x, y: (pos.y - offset.y)/scale.y }
-	}
-	function untransform(pos){
-		return { x: scale.x*pos.x + offset.x, y: scale.y*pos.y + offset.y }
 	}
 
 	function eachPairTwice(list, action){
@@ -172,118 +118,23 @@ function initNodes(canvasId, _options){
 		})
 
 		var springiness = springF
-		var springLength = natsep
-		relations.each(function (e, f){
+		relations.each(function (e, f, relation){
 			var d = dist(e, f)
-			var springForce = springiness * (d - springLength) / 10000
+			var springForce = relation.strength * springiness * (d - relation.length) / 10000
 			e.fx += (e.x - f.x)*(-springForce)
 			e.fy += (e.y - f.y)*(-springForce)
 		})
 	}
 
-	function drawEntities(){
-		g.ctx.lineWidth = 10
-		_.each(entities, function (e){
-			g.ctx.fillStyle = 'rgba(255, 255, 255, 0.75)'
-			g.circle(e.x, e.y, e.r+10).fill()
-			g.ctx.fillStyle = g.colorNorm(e.charge.r, e.charge.g, e.charge.b, 0.5)
-			g.circle(e.x, e.y, e.r).fill()
-		})
-	}
-
-	function drawRelations(){
-		var margin = 10
-		g.ctx.lineWidth = 5
-		g.ctx.strokeStyle = 'rgba(255, 255, 255, 0.5)'
-		relations.each(function (e1, e2){
-			var v = normalize(diff(e1, e2))
-			var p1 = [e1.x - v.x*(e1.r+margin), e1.y - v.y*(e1.r+margin)]
-			var p2 = [e2.x + v.x*(e2.r+margin), e2.y + v.y*(e2.r+margin)]
-			g.path([p1, p2]).stroke()
-		})
-	}
-
-	function withPickedEntity(pos, action){
-		var e = pickEntity(pos)
-		if (e)
-			action(e)
-	}
-
-	function pickEntity(pos){
-		var p = transform(pos)
-		var e = _.min(entities, function (e){ return dist(e, p) })
-		return (dist(e, p) < e.r + 10) ? e : undefined
-	}
-
-	var swe = [[104,496],[80,494],[75,471],[80,461],[55,418],[55,389],[61,382],[69,356],[75,340],
-	[70,323],[80,311],[67,294],[67,245],[81,214],[101,218],[102,201],[94,198],[108,138],[120,132],
-	[133,86],[156,53],[159,60],[166,37],[194,43],[201,12],[258,61],[264,151],[232,153],[213,190],
-	[223,206],[207,229],[162,264],[150,303],[154,337],[181,354],[178,366],[163,393],[143,403],
-	[143,448],[129,475],[108,477],[104,496]]
-
-	function draw(tick){
-		simulate()
-
-		g.background(150, 150, 150)
-
-		g.ctx.save()
-		g.ctx.translate(offset.x, offset.y)
-		g.ctx.scale(scale.x, scale.y)
-
-		withPickedEntity(g.mousePos(), function (e){
-			g.ctx.fillStyle = g.color255(0,0,0,0.25)
-			g.circle(e.x, e.y, e.r+20).fill()
-		})
-
-		if (clickedEntity){
-			var p = transform(g.mousePos())
-			var d = dist(clickedEntity, p)
-			var v = normalize(diff(p, clickedEntity))
-			if (d>10){
-				var path = _.times(Math.round(d/10), function (i){
-					return [
-						clickedEntity.x + i*v.x*10 + 10*v.y*Math.cos(i+tick/2),
-						clickedEntity.y + i*v.y*10 - 10*v.x*Math.cos(i+tick/2)
-					]
-				})
-				//g.path([[clickedEntity.x, clickedEntity.y], [mx, my]]).stroke()
-				g.ctx.strokeStyle = g.color255(255, 255, 255,0.5)
-				g.ctx.lineWidth = 13
-				g.path(path).stroke()
-			}
-		}
-
-		drawRelations()
-		drawEntities()
-
-		if (selectedEntity){
-			var e = selectedEntity
-			g.ctx.strokeStyle = g.color255(0,0,0,0.5)
-			g.ctx.lineWidth = 10/scale.x
-			var sidebarEdge = transform({x:g.width()-200, y:0})
-			g.path([[e.x + e.r + 5/scale.x, e.y], [sidebarEdge.x, e.y]]).stroke()
-		}
-
-		g.ctx.restore()
-	}
-
 	return {
-		unstabilize: function (t){
-			natsep = _.random(150, 250)
-		},
-		zoom: function(direction){
-			repeat(function(strength){
-				var f = 1 - 0.03*sq(strength)
-				scale.x *= direction > 0 ? f : 1/f
-				scale.y *= direction > 0 ? f : 1/f
-			})
-		},
-		fillScreen: function(){
-			var w = canvas.parentElement.offsetWidth
-			canvas.setAttribute('width', w)
-			canvas.setAttribute('height', w*3/4)
-			offset.x = g.width()/2
-			offset.y = g.height()/2
+		simulate: simulate,
+		entities: entities,
+		relations: relations,
+		addRelation: function (a, b){
+			var r = Relation(a.id, b.id)
+			r.strength = 0
+			repeat(function (v){ r.strength = v; console.log(v) }, 20)
+			relations.push(r)
 		}
 	}
 }
