86c2ca8aa5fee84b6693b1c5c991dc907ee94662
skanaar <daniel.kallin@gmail.com>
2014-03-11 00:05:35 +0100

    nomnoml: some tests

--- nomnoml/test/nomnoml.spec.js
+++ nomnoml/test/nomnoml.spec.js
@@ -1,12 +1,24 @@
 describe('nomnoml', function() {
 
-    describe('parser', function() {
-        it('sanity test of jison parser', function(){
+    describe('jison parser', function() {
+        it('should handle single relation', function(){
             var source = '[apa]->[banan]'
             var ast = parser.parse(source)
             expect(ast.type).toEqual('->')
             expect(ast.start).toEqual({parts:['apa']})
             expect(ast.end).toEqual({parts:['banan']})
+        })
+
+        it('should handle single class', function(){
+            var source = '[apa]'
+            var ast = parser.parse(source)
+            expect(ast).toEqual({parts:['apa']})
+        })
+
+        it('should handle single class with compartments', function(){
+            var source = '[apa|+field: int;#x:int|apply]'
+            var ast = parser.parse(source)
+            expect(ast).toEqual({parts:['apa','|','+field: int','#x:int','|','apply']})
         })
     })
 
