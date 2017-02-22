d40078956a114919a68902113862c7fa7afb8130
Daniel Kallin <daniel.kallin@gmail.com>
2017-02-22 22:06:02 +0100

    space rpg editor

---
+++ space-rpg/events.js
@@ -0,0 +1,24 @@
+if (!window.events) {
+  window.events = {}
+  riot.observable(window.events)
+}
+
+window.getWorld = function () {
+  var nullWorld = { destinations: [], quests: '', items: [], ships: [], enemies: [] }
+  try {
+    var world = JSON.parse(localStorage['world'])
+    if (!_.isArray(world.destinations)) throw new Error()
+    if (!_.isString(world.quests)) throw new Error()
+    if (!_.isArray(world.items)) throw new Error()
+    if (!_.isArray(world.ships)) throw new Error()
+    if (!_.isArray(world.enemies)) throw new Error()
+    return world
+  } catch(e) {
+    alert('locally stored world was corrupt, loading empty world instead')
+    return nullWorld
+  }
+}
+
+window.saveWorld = function (world) {
+  localStorage['world'] = _.stringify(world)
+}
