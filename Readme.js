fc5116bc4007b71e3f17dd6098ee23f7047d8bd2
skanaar <daniel.kallin@gmail.com>
2023-01-30 21:18:30 +0100

    tweaks

--- Readme.js
+++ Readme.js
@@ -10,9 +10,11 @@ export function Readme() {
     el(
       'p',
       { className: 'stack' },
+      el('h2', {}, 'Links'),
       el('a', { href: "http://www.rymdmotor.se" }, 'rymdmotor ab'),
       el('a', { href: "https://www.linkedin.com/in/danielkallin" }, 'linkedin'),
       el('a', { href: "daniel/cv.html" }, 'résumé'),
+      el('h2', {}, 'Projects'),
       el('a', { href: "http://www.nomnoml.com" }, 'nomnoml.com'),
       el('a', { href: "http://www.rymdmotor.se/nebula-sky" }, 'nebula sky'),
       el('a', { href: "deimos/editor.html#models" }, 'deimos'),
