1e3f6151662b8d42dcdd771aaa084b001fcec7fb
skanaar <daniel.kallin@gmail.com>
2024-11-05 22:53:23 +0100

    raytracer wave

--- Raytracer.js
+++ Raytracer.js
@@ -54,12 +54,26 @@ let add = ([a, b, c], [x, y, z]) => [a + x, b + y, c + z]
 let diff = ([a, b, c], [x, y, z]) => [a - x, b - y, c - z]
 let mult = (k, [a, b, c]) => [k*a, k*b, k*c]
 let dot = ([a, b, c], [x, y, z]) => a * x + b * y + c * z
+let cross = ([a, b, c], [x, y, z]) => [b*z-y*c, c*x-a*z, a*y-b*x]
 let mag = ([a, b, c]) => Math.sqrt(a * a + b * b + c * c)
 let div = ([a, b, c], k) => [a / k, b / k, c / k]
 let norm = (v) => div(v, mag(v))
+let rotx = (a, [x,y,z]) => [x, Math.cos(a)*y - Math.sin(a)*z, Math.sin(a)*y + Math.cos(a)*z]
 let sq = (x) => x * x
 let clamp = (x, low, high) => Math.max(low, Math.min(x, high))
 let rnd = (t = size) => t * Math.random()
+
+function wave(res, scale, periods) {
+  let point = (i,j) => [
+    scale*i,
+    scale*Math.cos(periods*3.14*2*(Math.sqrt(sq((i-res/2)/res)+sq((j-res/2)/res)))),
+    scale*j,
+  ]
+  return [...generateRowByRowCoordinates(res)].flatMap(([i,j]) => [
+    [point(i,j), point(i+1,j), point(i,j+1)],
+    [point(i+1,j), point(i+1,j+1), point(i,j+1)],
+  ])
+}
 
 let lights = [[200, 50, 128]]
 let spheres = []
@@ -70,8 +84,53 @@ let planes = [
   [[255,0,0], norm([-1,0,0.01])],
   [[0,0,-255], norm([0,0,1.01])],
 ]
-for (let i = 0; i < 5; i++)
-  spheres.push([[rnd(), rnd(), -256+rnd()], 32+rnd(32)])
+let triangles = offsetTriangles([20,200,100], rotateXTriangles(3.0, scaleTriangles(1, [
+  //[[1,0,0],[0,0,1],[0,1,0]],
+  //[[-1,0,0],[0,0,1],[0,1,0]],
+  //[[1,0,0],[0,0,-1],[0,1,0]],
+  //[[-1,0,0],[0,0,-1],[0,1,0]],
+  //[[1,0,0],[0,0,1],[0,-1,0]],
+  //[[-1,0,0],[0,0,1],[0,-1,0]],
+  //[[1,0,0],[0,0,-1],[0,-1,0]],
+  //[[-1,0,0],[0,0,-1],[0,-1,0]],
+
+  //[[0,0,1],[1,0,0],[0,1,0]],
+  //[[0,0,1],[-1,0,0],[0,1,0]],
+  //[[0,0,-1],[1,0,0],[0,1,0]],
+  //[[0,0,-1],[-1,0,0],[0,1,0]],
+  //[[0,0,1],[1,0,0],[0,-1,0]],
+  //[[0,0,1],[-1,0,0],[0,-1,0]],
+  //[[0,0,-1],[1,0,0],[0,-1,0]],
+  //[[0,0,-1],[-1,0,0],[0,-1,0]],
+
+  ...wave(5, 20, 3)
+])))
+//for (let i = 0; i < 5; i++)
+//  spheres.push([[rnd(), rnd(), -256+rnd()], 32+rnd(32)])
+
+function offsetTriangles(v, triangles) {
+  return triangles.map(([a,b,c]) => [add(a, v), add(b, v), add(c, v)])
+}
+function scaleTriangles(k, triangles) {
+  return triangles.map(([a,b,c]) => [mult(k, a), mult(k, b), mult(k, c)])
+}
+function rotateXTriangles(rot, triangles) {
+  return triangles.map(([a,b,c]) => [rotx(rot,a), rotx(rot,b), rotx(rot,c)])
+}
+
+function bezierPatch([a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p]) {
+}
+function bezier3D([a,b,c,d], t) {
+  return [
+    bezier1D(a[0], b[0], c[0], d[0], t),
+    bezier1D(a[1], b[1], c[1], d[1], t),
+    bezier1D(a[2], b[2], c[2], d[2], t),
+  ]
+}
+function bezier1D([a,b,c,d], t) {
+  let u = 1-1
+  return u*u*u*a + 3*u*u*t*b +3*u*t*t*c + t*t*t*d
+}
 
 function raytrace(canvas, size, spheres, lights) {
   let ctx = canvas.getContext("2d")
@@ -84,6 +143,7 @@ function raytrace(canvas, size, spheres, lights) {
 
   let camera = [128, 128, 512]
 
+  let start = performance.now()
   for (let [i,j] of ditherer.coordinates(size)) {
     let camDir = norm(diff([i, j, 0], camera))
     let hit = { depth: 1000, normal: null, point: [0,0,0] }
@@ -109,6 +169,23 @@ function raytrace(canvas, size, spheres, lights) {
       hit = { depth: t, normal, point: add(camera, mult(t, camDir)) }
     }
 
+    // triangle intersections
+    for (let [a,b,c] of triangles) {
+      let normal = norm(cross(diff(c,a), diff(b,a)))
+      if (dot(camDir, normal) > 0) continue
+      let t = (dot(a,normal) - dot(camera,normal)) / dot(camDir,normal)
+      if (t > hit.depth) continue
+      const p = add(camera, mult(t, camDir))
+      // is p outside triangle?
+      let to_a = cross(diff(c,b), normal)
+      let to_b = cross(diff(a,c), normal)
+      let to_c = cross(diff(b,a), normal)
+      if (dot(diff(p,b), to_a) < 0) continue
+      if (dot(diff(p,c), to_b) < 0) continue
+      if (dot(diff(p,a), to_c) < 0) continue
+      hit = { depth: t, normal, point: p }
+    }
+
     // hit patch illumination
     let bright = 0
     if (hit.normal != null)
@@ -118,6 +195,7 @@ function raytrace(canvas, size, spheres, lights) {
     ctx.fillStyle = toColor(ditherer.apply(bright, [i, j]))
     ctx.fillRect(i, j, 1, 1)
   }
+  console.log(`duration: ${(performance.now() - start).toFixed(0)}ms`)
 
   function toColor(value) {
     let c = Math.min(255, Math.max(0, value * 255))
@@ -127,7 +205,7 @@ function raytrace(canvas, size, spheres, lights) {
   function lightBrightness(p, surface_normal, light) {
     let light_dir = norm(diff(light, p))
     let dist = mag(diff(p, light))
-    let intensity = 40000/sq(dist)
+    let intensity = 30000/sq(dist)
     return Math.max(0, intensity * dot(surface_normal, light_dir))
   }
 }
