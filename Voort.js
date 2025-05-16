1f568357a948a0101ebd3f0a2189369e08eef989
skanaar <daniel.kallin@gmail.com>
2025-05-16 09:11:23 +0200

    tests, rename to Voort

--- Voort.js
+++ Voort.js
@@ -1,12 +1,40 @@
 import { el, App, useEvent } from './assets/system.js'
 import { useScript } from './assets/useScript.js'
 
-export const app = new App('Forward', Forward, 'cube.svg')
+export const app = new App('Voort', Voort, 'voort.svg')
 
 const LOOP_START = -1
 const LOOP_COUNT = -2
 
-function interpret(source, out) {
+function test(source, rawExpected) {
+  let actual = null
+  interpret(source, { out: e => actual = e })
+  let expected = JSON.stringify(rawExpected)
+  if (actual !== expected)
+    throw new Error(`got ${actual} expected ${expected} from "${source}"`)
+  else console.log(`test passed ${source}`)
+}
+
+function testsuite() {
+  test('7 3 + .', 10)
+  test('7 3 - .', 4)
+  test('7 3 / .', 7/3)
+  test('7 3 / .', 7/3)
+  test('7 3 .', 3)
+  test('7 3 drop .', 7)
+  test('7 3 swap .', 7)
+  test('7 3 swap swap .', 3)
+  test('2 dup + .', 4)
+  test('7 3 true ? .', 7)
+  test('7 3 true not ? .', 3)
+  test('7 neg .', -7)
+  test('3 true if 7 ; .', 7)
+  test('3 false if 7 ; .', 3)
+  test('4 { i }loop + + + .', 3+2+1)
+  test('"foo" : 7 ; foo .', 7)
+}
+
+function interpret(source, { out, trace }) {
   const tokens = source.split(/[ \n]+/)
   const stack = []
   const ctrl = []
@@ -17,8 +45,8 @@ function interpret(source, out) {
   const peek = () => stack.at(-1)
   let index = 0
   const evaluate = (token) => {
-    console.log(token, JSON.stringify({ stack, ctrl, dict }))
-    if (ctrl.at(-1) == 'skip;') {
+    if (trace) console.log(token, JSON.stringify({ stack, ctrl, dict }))
+    if (ctrl.at(-1) == 'skip_to_;') {
       if (token === ';') ctrl.pop()
       return
     }
@@ -35,14 +63,16 @@ function interpret(source, out) {
     if (token == '...') out(JSON.stringify(stack))
     if (token == '..c') out(JSON.stringify(ctrl))
     if (token == '..d') out(JSON.stringify(dict))
+    if (token == 'drop') pop()
     if (token == '+') push(pop() + pop())
     if (token == '-') push(-pop() + pop())
     if (token == '*') push(pop() * pop())
-    if (token == '/') push((1/pop()) * pop())
+    if (token == '/') { let d = pop(); push(pop() / d) }
+    if (token == 'pow') { const exp = pop(); push(Math.pow(pop(), exp)) }
     if (token == '=') push(pop() === pop())
     if (token == '>') push(pop() > pop())
     if (token == '<') push(pop() < pop())
-    if (token == 'swap') { const t = pop(); push(pop(), t) }
+    if (token == 'swap') { const a = pop(), b = pop(); push(a); push(b) }
     if (token == 'dup') push(peek())
     if (token == 'neg') push(-pop())
     if (token == 'not') push(!pop())
@@ -50,7 +80,7 @@ function interpret(source, out) {
       let condition = pop(), a = pop(), b = pop()
       push(condition === true ? b : a)
     }
-    if (token == 'if') if (pop() !== true) ctrl.push('skip;')
+    if (token == 'if') if (pop() !== true) ctrl.push('skip_to_;')
     if (token == ':') {
       ctrl.push(peek())
       ctrl.push('define')
@@ -71,21 +101,24 @@ function interpret(source, out) {
 
     if (token == 'true') push(true)
     if (token == 'false') push(false)
-    if (token.match(/^[0-9]+$/)) push(+token)
-    if (token.match(/"[0-9a-zA-Z]*"/)) push(token.replaceAll('"', ''))
+    if (token.match(/^[0-9.]+$/)) push(+token)
+    if (token.match(/"[^"]*"/)) push(token.replaceAll('"', ''))
   }
   for (; index<tokens.length; index++) {
     evaluate(tokens[index])
   }
 }
 
-function Forward() {
+function Voort() {
   const [source, setSource] = React.useState('10 { i 5 - dup * neg 25 + }loop ...')
   const [out, setOut] = React.useState('')
+  const [trace, setTrace] = React.useState(false)
   const onChange = (e) => setSource(e.target.value)
   const onRun = (e) => {
-    interpret(source, setOut)
+    interpret(source, { out: setOut, trace })
   }
+
+  React.useEffect(testsuite, [])
 
   return el('forward-app', { class: 'padded' },
     el('style', {}, css),
@@ -93,7 +126,16 @@ function Forward() {
       el('textarea', { value: source, onChange }),
     ),
     el('span', { class: 'console' }, out),
-    el('button', { className: 'btn', onClick: onRun }, 'Run'),
+    el('div', { style: { display: 'flex', alignItems: 'center', gap: 10 } },
+      el('button', { className: 'btn', onClick: onRun }, 'Run'),
+      el('label', {},
+        el('input', {
+          type: 'checkbox',
+          onChange: (e) => setTrace(e.target.checked)
+        }),
+        el('span', {}, 'Trace')
+      )
+    )
   )
 }
 
